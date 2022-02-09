2f7c313414f909505859a90d9e6cd2cbbd6bd0ca
GABRIELA CASTILLO ARECO <gabriela.castillo.areco@gmail.com>
2022-02-09 12:09:26 +0100

    authentication

--- app.py
+++ app.py
@@ -11,8 +11,16 @@ import random
 import dash
 from dash import html
 import dash_bootstrap_components as dbc
+import dash_auth
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}],
+                external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
+
+auth= dash_auth.BasicAuth (
+    app,
+    {'Hayek':'Francisco'}
+)
 
 # ------------------------------------------------------------
 # URL
@@ -290,10 +298,6 @@ df_volume.sort_values(by='solvency', ascending=False, inplace= True)
 
 print ('data gathered. ready to plot')
 # -----------------------------------------------
-
-app = dash.Dash(__name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}],
-                external_stylesheets=[dbc.themes.BOOTSTRAP])
-server = app.server
 
 tab_style = {"background": "darkred",'color': 'white',
              'text-transform': 'uppercase',
